8a551c23b0da7c2f133316e35d8d4d84aec6e080
eguono imonieroh <imonieroheguono@gmail.com>
2025-06-26 12:43:32 +0100

    feat:index.jsx,View,Text,Image components

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import bookImg from "../assets/img/book-circle.webp";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={bookImg} style={styles.img}/>
+      <Text style={styles.title}>The Best</Text>
+      <Text style={{marginTop:10, marginBottom:30}}>Book App</Text>
+      {/* <View style={styles.card}>
+        <Text>
+          Hello, this is a card.
+        </Text>
+      </View> */}
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  title:{
+    fontWeight:"bold",
+    fontSize:18
+  },
+  // card:{
+  //   backgroundColor:"#eee",
+  //   padding:20,
+  //   borderRadius:5,
+  //   boxShadow:"4px 4px rgba(0,0,0,0.1)"
+  // },
+  img:{
+    marginVertical:20,
+    width:250, 
+    height:250, 
+    borderRadius:50
+
+  }
+})
